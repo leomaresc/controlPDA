eebc4f3710a5ac5f8067b818f74c9205d7b6c260
leomaresc <leomarsalazarec@gmail.com>
2024-01-23 17:06:09 -0300

    TODO: Generar un objeto del formulario de agregar error.

--- app/AddError.js
+++ app/AddError.js
@@ -2,24 +2,50 @@ import { Text, View, TextInput } from 'react-native';
 import { useState } from 'react';
 import { SelectList } from 'react-native-dropdown-select-list';
 import { rutasLista, categorias, error } from '../rutas';
+import Button from '../src/components/Button';
 
 export default function AddError({route, navigate}){
 
-    const [selected, setSelected] = useState("")
     const [ user ] = [route.params]
+    const [selected, setSelected] = useState("")
+    const [mistake, setMistake] = useState({})
+    let updateMistake = {
+        nombre: user.name,
+        ruta: "",
+        categoria: "",
+        error: "",
+        observaciones: ""
+    }
 
     return(
         <View>
             <Text>Agregar error a {user.name}</Text>
 
             <Text>Selecciona ruta: </Text>
-            <SelectList setSelected={(x) => setSelected(x)} data={rutasLista} save="ruta"/>
+
+            <SelectList setSelected={(ruta) => {
+                updateMistake.ruta = ruta;
+                setMistake(updateMistake);
+                setSelected(ruta);
+                }} 
+            data={rutasLista} save="ruta" search={false} placeholder='Seleccione ruta'/>
+
             <Text>Categoría: </Text>
-            <SelectList data={categorias}/>
+
+            <SelectList setSelected={(categoria) => {
+                updateMistake.categoria = categoria;
+                setMistake(updateMistake);
+                setSelected(categoria);
+                }} 
+            data={categorias} search={false} save='categoria' placeholder='Seleccione categoría de la ruta'/>
+
             <Text>Tipo de error: </Text>
-            <SelectList data={error}/>
+
+            <SelectList setSelected={(error) => setSelected(error)} data={error} search={false} save='error' placeholder='Seleccione el tipo de error'/>
             <Text>Observaciones: </Text>
-            <TextInput style={{borderWidth: 5, borderColor: "Black"}} />
+            <TextInput style={{borderWidth: 5, borderColor: "Black"}} placeholder='Agregue una observación (opcional)' />
+
+            <Button text="Aceptar" textColor="white" buttonColor="#8CBED6" width="100%" height="100%" buttonFunction={()=> console.log(mistake)}/>
         </View>
     )
 }
