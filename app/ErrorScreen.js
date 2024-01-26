c2c50a12bb8490491532c4eba26697f3aa41a035
leomaresc <leomarsalazarec@gmail.com>
2024-01-26 14:27:54 -0300

    Hay que arreglar el singa su madre servidor. || Oficina

--- app/ErrorScreen.js
+++ app/ErrorScreen.js
@@ -1,6 +1,18 @@
 import { Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
 
 export default function ErrorScreen({ route, navigation }){
+
+    const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        fetch('https://localhost:3000/')
+          .then((resp) => resp.json())
+          .then((json) => setData(json))
+          .catch((error) => console.error(error))
+          .finally(() => setLoading(false));
+      }, []);
 
     const [ user ] = [route.params]
     console.log(user.name)
