5703258980c620d088b4689e9f1b22691bb0d21f
leomaresc <leomarsalazarec@gmail.com>
2024-01-26 15:43:17 -0300

    Crear interacción entre la app y el servidor. || Oficina.

--- app/ErrorScreen.js
+++ app/ErrorScreen.js
@@ -7,7 +7,7 @@ export default function ErrorScreen({ route, navigation }){
     const [loading, setLoading] = useState(true);
 
     useEffect(() => {
-        fetch('https://localhost:3000/')
+        fetch('http://10.101.46.136:3000/getErrorsByDate?fecha=24-01-2024')
           .then((resp) => resp.json())
           .then((json) => setData(json))
           .catch((error) => console.error(error))
@@ -15,11 +15,13 @@ export default function ErrorScreen({ route, navigation }){
       }, []);
 
     const [ user ] = [route.params]
-    console.log(user.name)
 
     return(
         <View>
             <Text>Ver todos los errores de {user.name}</Text>
+            {data.forEach(element => {
+                return <Text>{element.nombre}</Text>
+            })}
         </View>
     )
 }
